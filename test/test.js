8d428050d28b3def4b1b73b95b559a6f162c8af2
Ryan Day <soldair@gmail.com>
2014-06-09 18:39:02 -0700

    chaning board pinoccio to work with @AdamMagaluk's interface change

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var test = require('tape');
 var board = require('../');
 
 test("can load pinoccio board",function(t){
-  var b = board();
+  var b = board['pinoccio'];
 
   t.ok(b,'should export a board');
   t.equals(b.name,'Pinoccio Scout','should have correct name');
